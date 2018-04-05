3cb98d7cff998157bf48c1a36d3cd3e1b5467f22
vikasdesale <vikas.desaleu@gmail.com>
2018-04-05 22:14:40 +0530

    fix: #25 Symbols

---
+++ 2.Javascript with ES6/9.symbols.js
@@ -0,0 +1,7 @@
+const bowl = {
+  [Symbol('apple')]: { color: 'red', weight: 136.078 },
+  [Symbol('banana')]: { color: 'yellow', weight: 183.15 },
+  [Symbol('orange')]: { color: 'orange', weight: 170.097 },
+  [Symbol('banana')]: { color: 'yellow', weight: 176.845 }
+};
+console.log(bowl);
