7510f10ce6d1a70bd03aa803c6d2da128748a6de
vikasdesale <vikas.desaleu@gmail.com>
2018-04-07 21:46:45 +0530

    fix: #31 Looping through maps

---
+++ 2.Javascript with ES6/14.Looping through maps.js
@@ -0,0 +1,15 @@
+/*
+ * Using array destructuring, fix the following code to print the keys and values of the `members` Map to the console.
+ */
+
+const members = new Map();
+
+members.set('Evelyn', 75.68);
+members.set('Liam', 20.16);
+members.set('Sophia', 0);
+members.set('Marcus', 10.25);
+
+for (const member of members) {
+    const [key, value] = member;
+    console.log(key, value);
+}
