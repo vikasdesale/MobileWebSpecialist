d42d11374e569c91b3f8a52313135f6bf7a6e640
vikasdesale <vikas.desaleu@gmail.com>
2018-04-07 21:54:23 +0530

    fix: #33 Promises

---
+++ 2.Javascript with ES6/16.Promises.js
@@ -0,0 +1,13 @@
+new Promise(function (resolve, reject) {
+    window.setTimeout(function createSundae(flavor = 'chocolate') {
+        const sundae = {};
+        // request ice cream
+        // get cone
+        // warm up ice cream scoop
+        // scoop generous portion into cone!
+        if ( /* iceCreamConeIsEmpty(flavor) */ ) {
+            reject(`Sorry, we're out of that flavor :-(`);
+        }
+        resolve(sundae);
+    }, Math.random() * 2000);
+});
