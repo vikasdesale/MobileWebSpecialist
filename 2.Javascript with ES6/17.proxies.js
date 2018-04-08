41995abd5d0f8dd43b79592a8d93cab3f192f474
vikasdesale <vikas.desaleu@gmail.com>
2018-04-08 21:48:26 +0530

    fix: #34 Proxies

---
+++ 2.Javascript with ES6/17.proxies.js
@@ -0,0 +1,57 @@
+//A Pass Through Proxy
+//The simplest way to create a proxy is to provide an object and then an empty handler object.
+
+var richard = {status: 'looking for work'};
+var agent = new Proxy(richard, {});
+
+agent.status; // returns 'looking for work'
+
+//Get Trap
+//The get trap is used to "intercept" calls to properties:
+
+const richard = {status: 'looking for work'};
+const handler = {
+    get(target, propName) {
+        console.log(target); // the `richard` object, not `handler` and not `agent`
+        console.log(propName); // the name of the property the proxy (`agent` in this case) is checking
+    }
+};
+const agent = new Proxy(richard, handler);
+agent.status; // logs out the richard object (not the agent object!) and the name of the property being accessed (`status`)
+
+//Accessing the Target object from inside the proxy
+//If we wanted to actually provide the real result, we would need to return the property on the target object:
+
+const richard = {status: 'looking for work'};
+const handler = {
+    get(target, propName) {
+        console.log(target);
+        console.log(propName);
+        return target[propName];
+    }
+};
+const agent = new Proxy(richard, handler);
+agent.status; // (1)logs the richard object, (2)logs the property being accessed, (3)returns the text in richard.status
+
+//Alternatively, we could use the proxy to provide direct feedback:
+
+const richard = {status: 'looking for work'};
+const handler = {
+    get(target, propName) {
+        return `He's following many leads, so you should offer a contract as soon as possible!`;
+    }
+};
+const agent = new Proxy(richard, handler);
+agent.status; // returns the text `He's following many leads, so you should offer a contract as soon as possible!`
+
+
+
+const proxyObj = new Proxy({age: 5, height: 4}, {
+    get(targetObj, property) {
+        console.log(`getting the ${property} property`);
+        console.log(targetObj[property]);
+    }
+});
+
+proxyObj.age; // logs 'getting the age property' & 5
+proxyObj.height; // logs 'getting the height property' & 4
